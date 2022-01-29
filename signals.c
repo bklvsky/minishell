157662946c1a840bdd7a0548f7690b5f199102f0
sstyr <smoooky2012@gmail.com>
2022-01-29 15:23:21 +0300

    added heredoc and child signal processing

--- signals.c
+++ signals.c
@@ -8,6 +8,17 @@ void	sig_int(int signal)
 		rl_on_new_line();
 		rl_replace_line("", 1);
 		rl_redisplay();
-//		g_exit_status = 1;
 	}
+}
+
+void	here_sig(int signal)
+{
+	if (signal == SIGINT)
+		exit(1);
+}
+
+void	child_sig(int signal)
+{
+	if (signal == SIGINT)
+		write(1, "\n", 1);
 }
